416545d6d0bafb87141df9cfef09040d5b85c108
Pierre Kisters <pierrekisters@gmail.com>
2022-10-13 22:50:48 +0200

    run http request

--- build.sbt
+++ build.sbt
@@ -8,8 +8,9 @@ lazy val root = (project in file("."))
     name := "httpclient",
 
     libraryDependencies ++= Seq(
-      //"com.lihaoyi" %%% "mainargs" % "0.3.0",
-      "com.github.alexarchambault" %% "case-app-cats" % "2.1.0-M18",
+      "com.github.alexarchambault" %% "case-app" % "2.1.0-M18",
       "org.http4s" %%% "http4s-curl" % "0.1.1",
-    )
+    ),
+
+    addCompilerPlugin("com.olegpy" %% "better-monadic-for" % "0.3.1")
   )
--- src/main/scala/de/lhns/httpclient/Main.scala
+++ src/main/scala/de/lhns/httpclient/Main.scala
@@ -1,34 +1,71 @@
 package de.lhns.httpclient
 
+import caseapp.Parser
 import caseapp.cats.IOCaseApp
+import caseapp.core.Error.MalformedValue
 import caseapp.core.RemainingArgs
 import caseapp.core.app.CaseApp
 import caseapp.core.argparser.{ArgParser, SimpleArgParser}
+import cats.data.EitherT
 import cats.effect._
 import cats.syntax.all._
-import org.http4s.{Method, Request}
-import org.http4s.curl.CurlApp
+import org.http4s.{Header, Headers, Method, Request, Uri}
 import cats.syntax.either._
+import org.http4s.curl.CurlApp
+import cats.syntax.functor._
+import cats.syntax.flatMap._
+import org.typelevel.ci.CIString
 
-case class Options(method: String,
+case class Options(
+                    method: Method,
+                    url: Uri,
+                    header: Seq[Header.Raw]
                   )
 
 object Options {
-  //implicit val customArgParser: ArgParser[Method] =
-  //  SimpleArgParser.from[String]("http method")(Method.fromString(_).leftMap(e =>))
+  private implicit val methodParser: ArgParser[Method] = SimpleArgParser.from("http method")(
+    Method.fromString(_)
+      .leftMap(e => MalformedValue("http method", e.message))
+  )
+
+  private implicit val uriParser: ArgParser[Uri] = SimpleArgParser.from("url")(
+    Uri.fromString(_)
+      .leftMap(e => MalformedValue("url", e.message))
+  )
+
+  private implicit val headerParser: ArgParser[Header.Raw] = SimpleArgParser.from("http header")(string =>
+    string.split("\\s*:\\s*", 2).toSeq match {
+      case Seq(name, value) => Right(Header.Raw(CIString(name), value))
+      case _ => Left(MalformedValue("http header", "must match pattern `name:value`"))
+    }
+  )
+
+  implicit val parser: Parser[Options] = {
+    import caseapp._
+    Parser[Options]
+  }
 }
 
-object Main extends IOCaseApp[Options] with CurlApp {
-  override def run(options: Options, remainingArgs: RemainingArgs): IO[ExitCode] = {
-    println(options.toString).as(ExitCode.Success)
-  }
-  /*override def run(args: List[String]): IO[ExitCode] = {
-    CaseApp.parse[Options](args)
-    for {
-      responses <- curlClient.stream(Request[IO](
-
-      ))
-      _ <- responses.traverse_(r => IO.println(r.joke))
-    } yield ()
-  }*/
+object Main extends CurlApp {
+  override def run(args: List[String]): IO[ExitCode] =
+    (for {
+      (options, _) <- EitherT(IO(CaseApp.parse[Options](args)))
+        .leftMap(_.message)
+      _ <- EitherT.right[String](curlClient.run(Request[IO](
+        method = options.method,
+        uri = options.url,
+        headers = Headers(options.header.map[Header.ToRaw](e => e): _*)
+      )).use { response =>
+        response.body
+          .through(
+            if (response.status.isSuccess)
+              fs2.io.stdout
+            else
+              fs2.io.stderr
+          )
+          .compile
+          .drain
+      })
+    } yield ())
+      .fold(_ => ExitCode.Error, _ => ExitCode.Success)
 }
